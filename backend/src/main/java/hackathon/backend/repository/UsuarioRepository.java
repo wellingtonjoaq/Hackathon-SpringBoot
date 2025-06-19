1a1a4fa1cf3b3ef58dc9b4cf9da96cf0bb7f35e7
ArthurT09 <arthurtomba@gmail.com>
2025-06-19 15:56:03 -0300

    criados alguns templates, banco gerando as tabelas.

--- backend/src/main/java/hackathon/backend/repository/UsuarioRepository.java
+++ backend/src/main/java/hackathon/backend/repository/UsuarioRepository.java
@@ -2,6 +2,8 @@ package hackathon.backend.repository;
 
 import hackathon.backend.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
 }
